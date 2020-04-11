0c5d321ecd174b756e6daa9d2c0021d4fa9366e3
mgx0612 <36354243+mgx0612@users.noreply.github.com>
2020-04-11 17:30:52 +0800

    try twitter client

---
+++ TwittertBot/ConsoleClientTest/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using TwittertBot.Client;
+
+namespace ConsoleClientTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var t = ClientImpl.FindPopularTweetByHashTag("#justsaying");
+            Console.WriteLine(t);
+        }
+    }
+}
---
+++ TwittertBot/TwittertBot.Client/ClientImpl.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using Tweetinvi;
+using Tweetinvi.Models;
+using Tweetinvi.Parameters;
+
+namespace TwittertBot.Client
+{
+    public static class ClientImpl
+    {
+        public static ITweet FindPopularTweetByHashTag(string key, string secret, string token, string hashtag)
+        {
+            Auth.SetApplicationOnlyCredentials(key, secret, token);
+            ISearchTweetsParameters searchParams = Search.CreateTweetSearchParameter(hashtag);
+            searchParams.SearchType = SearchResultType.Popular;
+            searchParams.MaximumNumberOfResults = 1;
+            var ts = Search.SearchTweets(searchParams);
+            return ts.Any() ? ts.First() : null;
+        }
+
+        public static ITweet FindPopularTweetByHashTag(string hashtag)
+        {
+            var key = "api key";
+            var secret = "api secret";
+            var token = "bearer token";
+            return FindPopularTweetByHashTag(key, secret, token, hashtag);
+        }
+    }
+}
